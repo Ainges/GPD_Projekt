a2bdc90bab82408550332fe02bafb15ffba356e9
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-05-25 17:35:31 +0200

    🔧generated by IDE

--- src/test/java/de/thi/ProcessUnitTest.java
+++ src/test/java/de/thi/ProcessUnitTest.java
@@ -1,5 +1,6 @@
 package de.thi;
 
+import de.thi.autogen.ProcessConstants;
 import org.apache.ibatis.logging.LogFactory;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
@@ -11,7 +12,6 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
-import static org.junit.Assert.*;
 
 /**
  * Test case starting an in-memory database-backed Process Engine.
