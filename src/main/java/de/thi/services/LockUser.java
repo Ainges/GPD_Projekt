52f4594e674dada5dbe22b742869278c4298aa7b
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-05-30 19:08:41 +0200

    🚧2 Escalation Paths Added

---
+++ src/main/java/de/thi/services/LockUser.java
@@ -0,0 +1,24 @@
+package de.thi.services;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LockUser implements JavaDelegate {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LockUser.class);
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+
+        LOG.info("!!!!!!!!!!!!!!!!!!!!!");
+        LOG.info("Locking User: "
+                + execution.getVariable("vorname")
+                + " "
+                + execution.getVariable("nachname")
+                + " ...");
+        LOG.info("!!!!!!!!!!!!!!!!!!!!!");
+
+    }
+}
